5a2085126614a91fab1241a1d8578550a3ff08cc
SAINT-MARC Kelana [EIFFAGE ENERGIE SYSTEMES] <Kelana.SAINT-MARC@eiffage.com>
2024-06-25 08:51:10 +0200

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Menu from './Menu';
 import About from './Pages/About';
 import Home from './Pages/Home';
