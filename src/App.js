e0139a082cabd7d8ecf51b68ae3d0ac7d61df4e9
SAINT-MARC Kelana [EIFFAGE ENERGIE SYSTEMES] <Kelana.SAINT-MARC@eiffage.com>
2024-06-26 13:14:04 +0200

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import './App.css';
-import { Routes, Route} from "react-router-dom";
-import About from "./routes/About";
-import Careers from "./routes/Careers";
-import Home from "./routes/Home";
-import Navbar from './Navbar';
+import {Route, Routes } from 'react-router-dom';
+import Menu from './Menu';
+import About from './routes/About';
+import Home from './routes/Home';
+import Contact from './routes/Contact';
+import Dessins from './routes/Dessins';
+import Dev from './routes/Dev';
+
+
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/careers" element={<Careers />} />
+      <Menu></Menu>
+      <Routes id='page'>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/experience" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/art" element={<Dessins />} />
+        <Route path="/projects" element={<Dev />} />
       </Routes>
     </>
   );
 }
+
 export default App;
