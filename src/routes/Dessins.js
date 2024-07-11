6620c3a3c5ac2c5dd1dc235d3369cb5ca2e4133a
SAINT-MARC Kelana [EIFFAGE ENERGIE SYSTEMES] <Kelana.SAINT-MARC@eiffage.com>
2024-07-11 13:12:04 +0200

    Ajout modale pour description art pieces

--- src/routes/Dessins.js
+++ src/routes/Dessins.js
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import InfoArray from "../json/art.json";
+import { Modal } from "@mui/material";
 
 function Dessins() {
+  const [modal, setModal] = useState(null)
   return (
     <div className="App">
       <div className="gallery">
+        {modal && <Modal open={modal} onClose={()=>setModal(null)}>
+          <div className="art-modal">
+            <h1>{modal.title}</h1>
+            <div>
+              <img className="artpic" src={"/img/art" + modal.path} alt={modal.description}/>
+              <p>{modal.description}</p>
+            </div>
+          </div>
+        </Modal>}
       {InfoArray.Art.map(elem => (
         <>
-          <img className="artpic" src={"/img/art" + elem.path} alt={elem.description}/>
+          <img onClick={()=>setModal(elem)} className="artpic" src={"/img/art" + elem.path} alt={elem.description}/>
         </>
       ))}</div>
     </div>
